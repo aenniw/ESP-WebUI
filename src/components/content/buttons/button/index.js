24fcb6842db324cc292e004c10d3178682416f64
Martin Mihálek <aenniw@gmail.com>
2019-03-30 11:15:24 +0100

    Add reactive header menu
    Signed-off-by: Martin Mihálek <aenniw@gmail.com>

--- src/components/content/buttons/button/index.js
+++ src/components/content/buttons/button/index.js
@@ -3,6 +3,15 @@ import { h } from "preact";
 import { Text } from "preact-i18n";
 import style from "./style.less";
 
-export default function Button({ label, name = <Text id={label} />, onClick }) {
-  return <button onClick={onClick}>{name && name}</button>;
+export default function Button({
+  className,
+  label,
+  name = <Text id={label} />,
+  onClick
+}) {
+  return (
+    <button onClick={onClick} class={className}>
+      {name && name}
+    </button>
+  );
 }
--- src/components/content/header/index.js
+++ src/components/content/header/index.js
@@ -1,16 +1,72 @@
 // noinspection ES6UnusedImports
 import { Component, h } from "preact";
-import { Link } from "preact-router";
 import style from "./style.less";
 
+import { Navigation, NavigationMenu, NavigationDropDown } from "./navigation";
+
 export default class Header extends Component {
-  render({ children, label }, {}) {
+  state = { pressed: false };
+
+  toggle = () => {
+    this.setState(({ pressed }) => {
+      return {
+        pressed: !pressed
+      };
+    });
+  };
+
+  release = () => {
+    this.setState({ pressed: false });
+  };
+
+  navClassName = href => {
+    if (this.props.url === href) return style.selected;
+    return style.focus;
+  };
+
+  navInnerClassName = href => {
+    if (this.props.url === href) return style.selected_mobile;
+    return style.hidden;
+  };
+
+  render({ hrefs = ["/gpio/digital", "/led-strip"] }, { pressed }) {
     return (
-      <header class={style.header}>
-        <h1>
-          <Link href="/">{label}</Link>
-        </h1>
-        <nav>{children}</nav>
+      <header className={pressed ? style.header_expanded : style.header}>
+        <Navigation
+          onClick={this.release}
+          className={this.navClassName}
+          href="/"
+          label="home"
+        />
+        <Navigation
+          onClick={this.release}
+          className={this.navClassName}
+          href="/config"
+        />
+        <NavigationDropDown
+          onClick={this.release}
+          className={this.navClassName}
+          label="navigation.services"
+          hrefs={hrefs}
+        />
+        {hrefs.map(href => (
+          <Navigation
+            onClick={this.release}
+            className={this.navInnerClassName}
+            href={href}
+          />
+        ))}
+        <Navigation
+          onClick={this.release}
+          className={this.navClassName}
+          href="/monitoring"
+        />
+        <Navigation
+          onClick={this.release}
+          className={this.navClassName}
+          href="/logout"
+        />
+        {!pressed && <NavigationMenu onClick={this.toggle} />}
       </header>
     );
   }
